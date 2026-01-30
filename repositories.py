cf085d26ad257b290cd550ebdb46f58ea1cb1765
Anton Yakovenko <anya@itu.dk>
2026-01-30 14:49:23 +0100

    added group name and our public repository

--- repositories.py
+++ repositories.py
@@ -1,8 +1,8 @@
 GROUP_REPOS = [
     [
         "group a",
-        "<name>",
-        ["https://github.com/<organization|user>/<repository_name>"],
+        "EasTops",
+        ["https://github.com/AntohaY/itu-minitwit"],
         "http(s)://<TBA>/<FrontEndURL>",
         "http(s)://<TBA>/<APIURL>",
     ],
